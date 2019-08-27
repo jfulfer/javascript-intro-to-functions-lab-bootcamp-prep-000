88b48ce0c9e145a883fdbbfcce2ae76470291734
Jeff Fulfer <jfulfer3@gmail.com>
2019-08-27 13:59:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) {
     return "I can't hear you"
-  elseif (string == string.toUpperCase())
   }
-}
+  elseif (string == string.toUpperCase()) {
+    return "YES INDEED"
+  }
+  elseif (string == "I love you, Grandma") {
+    return "I love you, too"
+  }
+  }
